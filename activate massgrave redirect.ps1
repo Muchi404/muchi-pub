59e15aac55fd63fe2fc90bb7677b3d83fd708434
Muchi404 <vertuescloud@gmail.com>
2026-01-09 16:30:43 +1300

    Update activate massgrave redirect.ps1

--- activate massgrave redirect.ps1
+++ activate massgrave redirect.ps1
@@ -1,1 +1,20 @@
-Start-Process powershell -ArgumentList '-NoExit','-Command','irm https://get.activated.win | iex'
+# Variables
+$url = "https://raw.githubusercontent.com/Muchi404/muchi-pub/main/MAS_AIO.cmd"
+$downloadPath = "C:\Windows\Temp\MAS_AIO.cmd"
+$shortcutPath = [Environment]::GetFolderPath("Desktop") + "\HWID Activate.lnk"
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $downloadPath
+
+# Create a WScript.Shell COM object
+$wsh = New-Object -ComObject WScript.Shell
+
+# Create the shortcut
+$shortcut = $wsh.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $downloadPath
+$shortcut.Arguments = "/hwid"
+$shortcut.WorkingDirectory = "C:\Windows\Temp"
+$shortcut.Save()
+
+# Launch the shortcut
+Start-Process $shortcutPath
